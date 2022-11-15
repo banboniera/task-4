c86a47eecee87dfe8b18bf500677d8f7bc40c527
Banbo <53372474+banboniera@users.noreply.github.com>
2022-11-15 16:05:28 +0100

    Group 1 task 2

---
+++ group1/task2/main.py
@@ -0,0 +1,45 @@
+from operations import (Addition, Division, Multiplication, Operation,
+                        Subtraction)
+
+
+def main():
+    operations = []  # type: list[Operation]
+    operations.append(Addition)
+    operations.append(Subtraction)
+    operations.append(Multiplication)
+    operations.append(Division)
+
+    first_number = 0
+    second_number = 0
+    not_numeric = True
+
+    print("====================================================================================================")
+    print("")
+
+    while(not_numeric):
+        print("Please provide first number: ", end="")
+        first_number = input()
+        not_numeric = not first_number.isnumeric()
+    
+    not_numeric = True
+
+    while(not_numeric):
+        print("Please provide second number: ", end="")
+        second_number = input()
+        not_numeric = not second_number.isnumeric()
+
+    operation = ""
+    while(operation not in ["+", "-", "*", "/"]):
+        print("Please provide operation (+, -, *, /): ", end="")
+        operation = input()
+
+    for op in operations:
+        op.calculate(operation, first_number, second_number)
+
+    print("")
+    print("====================================================================================================")
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ group1/task2/operations.py
@@ -0,0 +1,50 @@
+class Operation:
+    first_number = 0
+    second_number = 0
+
+    def calculate(operation: str, first_number: float, second_number: float):
+        pass
+
+
+class Addition(Operation):
+    def calculate(operation, first_number, second_number):
+        first_number = float(first_number)
+        second_number = float(second_number)
+        if operation == "+":
+            print("{} + {} = {}".format(first_number,
+                  second_number, first_number + second_number))
+            return
+        pass
+
+
+class Subtraction(Operation):
+    def calculate(operation, first_number, second_number):
+        first_number = float(first_number)
+        second_number = float(second_number)
+        if operation == "-":
+            print("{} - {} = {}".format(first_number,
+                  second_number, first_number - second_number))
+            return
+        pass
+
+
+class Multiplication(Operation):
+    def calculate(operation, first_number, second_number):
+        first_number = float(first_number)
+        second_number = float(second_number)
+        if operation == "*":
+            print("{} * {} = {}".format(first_number,
+                  second_number, first_number * second_number))
+            return
+        pass
+
+
+class Division(Operation):
+    def calculate(operation, first_number, second_number):
+        first_number = float(first_number)
+        second_number = float(second_number)
+        if operation == "/":
+            print("{} / {} = {}".format(first_number,
+                  second_number, round(first_number / second_number, 2)))
+            return
+        pass
